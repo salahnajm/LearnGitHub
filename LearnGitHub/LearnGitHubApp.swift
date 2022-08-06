5a884183f4f191b14537e1586b188b374b5dbde3
Najm <salahnajm@gmail.com>
2022-08-06 10:18:59 -0500

    Initial Commit

---
+++ LearnGitHub/LearnGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGitHubApp.swift
+//  LearnGitHub
+//
+//  Created by Salah Najm on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
